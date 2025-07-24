2f939c6c103c2cbdeba4da7c993fcbd185d28a3f
seqwut <f.eggstein@gmail.com>
2025-07-24 13:47:44 +0000

    chhange repo + branch

--- tensorprox/core/round_manager.py
+++ tensorprox/core/round_manager.py
@@ -537,8 +537,8 @@ class RoundManager(BaseModel):
         ip: str,
         ssh_user: str,
         key_path: str,
-        repo_url: str = "https://github.com/seqwut/suppenkasper.git",
-        branch: str = "fiveminrun",
+        repo_url: str = "https://github.com/shugo-labs/tensorprox.git",
+        branch: str = "hyperscaler",
         sparse_folder: str = "tensorprox/core/immutable",
         timeout: int = 120
     ) -> bool:
