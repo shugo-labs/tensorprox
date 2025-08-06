b12037e80b52b67d41bddbbdc3678b261712067b
borgg-dev <data.deepmind@gmail.com>
2025-08-06 13:19:11 +0100

    increase burn rate to 0.95

--- tensorprox/global_vars.py
+++ tensorprox/global_vars.py
@@ -3,4 +3,4 @@ from tensorprox.rewards.reward import ChallengeRewardEvent
 # Define shared mutable globals
 reward_events: list[ChallengeRewardEvent] = []
 BURN_UID = 0
-BURN_WEIGHT = 0.95
+BURN_WEIGHT = 0.99
